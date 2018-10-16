7d47c0a33c654806253580f41cd8293c151e6b38
clinhares <cleiton@hiper.com.br>
2018-10-15 21:33:01 -0300

    estrutura do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
