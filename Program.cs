d63a0803e9361fc98a8a4337166fbf62cb726c55
clinhares <cleiton@hiper.com.br>
2018-10-15 21:37:01 -0300

    retorno projeto n3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace gitteste
+namespace ola_mundo
 {
+
+    public class funcao{
+        public string texto{get; set;}
+
+        public string retorno(){
+            string var1 = "fedcbA";
+            char[] var2;
+
+            var2 = var1.ToCharArray();
+
+            for(int i =0; i<var1.Length; i++){
+                Console.WriteLine("t "+var2[i]+" "+(int)var2[i] );
+            }
+            
+            //Console.WriteLine("teste "+var2[1]);
+
+
+            return "";
+
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            funcao fnc = new funcao();
+
+            fnc.retorno();
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine("ola mundo");
         }
     }
 }
